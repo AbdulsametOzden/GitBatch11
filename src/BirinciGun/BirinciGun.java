4ecedcf60765761870f822d863624ddd46ae0449
Abdulsamet <abdulsametozden0212@gmail.com>
2020-11-17 20:11:01 +0100

    Ilk commit

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -1,6 +1,6 @@
-package IlkPaket;
+package BirinciGun;
 
-public class IlkGünTekrar {
+public class BirinciGun {
 
 	public static void main(String[] args) {
 		System.out.println("11.57-Birinci Gün");
