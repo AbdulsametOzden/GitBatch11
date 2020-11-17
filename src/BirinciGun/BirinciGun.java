8c11a0f25e4c74e9da39ee3ecf25168faaa296a4
AbdulsametOzden <74271213+AbdulsametOzden@users.noreply.github.com>
2020-11-17 20:21:36 +0100

    Update BirinciGun.java

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -5,7 +5,7 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11.57-Birinci Gün");
 		System.out.println("ikinci commit");
-		System.out.println("10:11 - Ikinci Gun")
+		System.out.println("10:11 - Ikinci Gun");
 	}
 
 }
