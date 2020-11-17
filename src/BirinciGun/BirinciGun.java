8655b45fd9092a4e2413325367227b64549da365
Abdulsamet <abdulsametozden0212@gmail.com>
2020-11-17 21:17:05 +0100

    mesaj

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -3,11 +3,12 @@ package BirinciGun;
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		System.out.println("11.57-Birinci GÃ¼n");
+		System.out.println("11.57-Birinci Gun");
 		System.out.println("ikinci commit");
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - 2. gun");
 		System.out.println("10:35 -Ikinci Gun");
+		System.out.println("11:05 - Ikinci Gun");
 	}
 
 }
