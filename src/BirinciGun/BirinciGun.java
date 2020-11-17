654425c510d8cfdc947b70515bb5df5a00f3dc25
Abdulsamet <abdulsametozden0212@gmail.com>
2020-11-17 20:28:20 +0100

    Ikinci gun 2. commit

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -6,6 +6,7 @@ public class BirinciGun {
 		System.out.println("11.57-Birinci GÃ¼n");
 		System.out.println("ikinci commit");
 		System.out.println("10:11 - Ikinci Gun");
+		System.out.println("10:26 - 2. gün");
 	}
 
 }
