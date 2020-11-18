ac3d81d0dc537513596b938622572fb5537a3ee8
Abdulsamet <abdulsametozden0212@gmail.com>
2020-11-18 21:46:49 +0100

    mesaj

--- src/BirinciGun/BirinciGun.java
+++ src/BirinciGun/BirinciGun.java
@@ -11,6 +11,7 @@ public class BirinciGun {
 		System.out.println("11:05 - Ikinci Gun");
 		System.out.println("11:23 - github.com"); 
 		System.out.println("ben buradan conflig beklemiyorum");
+		System.out.println("Localden yepyeni bir satir");
 		System.out.println("yepyeni bir satir");
 		System.out.println("10:26 - feature branch");
 		System.out.println("10:52 - feature branch iki");
